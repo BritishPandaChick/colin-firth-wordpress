78cdb0188bb3603f8cb523066a72ab45bbbb068f
britishpandachick <sbartley1080@gmail.com>
2023-12-19 11:11:49 -0500

    Update site kit plugin. Activate wordfence and smush plugins. Set up main menu

---
+++ wp-content/wflogs/config.php
@@ -0,0 +1,6 @@
+<?php exit('Access denied'); __halt_compiler(); ?>
+******************************************************************
+This file is used by the Wordfence Web Application Firewall. Read 
+more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
+******************************************************************
+a:6:{s:9:"wafStatus";s:7:"enabled";s:30:"learningModeGracePeriodEnabled";i:0;s:7:"authKey";s:64:"-_/j(D/_a}QLw7NvPGt*nf=^23,ughoeK49W:RP$*9{Q*jrRqiTN37aDj{+.XDcr";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:3385;}
